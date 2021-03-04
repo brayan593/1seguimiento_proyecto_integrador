8316e0980a1115d94c5c8de6a7ee52e8d990ed59
brayan593 <baa.andrade@yavirac.edu.ec>
2021-03-04 03:54:41 -0500

    proyecto integrador

---
+++ PIpeertopeer/PIpeertopeer/Form1.Designer.cs
@@ -0,0 +1,120 @@
+﻿
+namespace PIpeertopeer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.txtUsuario = new System.Windows.Forms.TextBox();
+            this.btnConectar = new System.Windows.Forms.Button();
+            this.lbTitulo1 = new System.Windows.Forms.Label();
+            this.txtMensaje = new System.Windows.Forms.TextBox();
+            this.btnEnviar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(12, 12);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(370, 264);
+            this.listBox1.TabIndex = 0;
+            // 
+            // txtUsuario
+            // 
+            this.txtUsuario.Location = new System.Drawing.Point(103, 131);
+            this.txtUsuario.Name = "txtUsuario";
+            this.txtUsuario.Size = new System.Drawing.Size(179, 20);
+            this.txtUsuario.TabIndex = 1;
+            // 
+            // btnConectar
+            // 
+            this.btnConectar.Location = new System.Drawing.Point(147, 166);
+            this.btnConectar.Name = "btnConectar";
+            this.btnConectar.Size = new System.Drawing.Size(75, 23);
+            this.btnConectar.TabIndex = 2;
+            this.btnConectar.Text = "Conectar";
+            this.btnConectar.UseVisualStyleBackColor = true;
+            this.btnConectar.Click += new System.EventHandler(this.btnConectar_Click);
+            // 
+            // lbTitulo1
+            // 
+            this.lbTitulo1.AutoSize = true;
+            this.lbTitulo1.Location = new System.Drawing.Point(100, 95);
+            this.lbTitulo1.Name = "lbTitulo1";
+            this.lbTitulo1.Size = new System.Drawing.Size(56, 13);
+            this.lbTitulo1.TabIndex = 3;
+            this.lbTitulo1.Text = "USUARIO";
+            // 
+            // txtMensaje
+            // 
+            this.txtMensaje.Location = new System.Drawing.Point(12, 293);
+            this.txtMensaje.Name = "txtMensaje";
+            this.txtMensaje.Size = new System.Drawing.Size(270, 20);
+            this.txtMensaje.TabIndex = 4;
+            // 
+            // btnEnviar
+            // 
+            this.btnEnviar.Location = new System.Drawing.Point(302, 293);
+            this.btnEnviar.Name = "btnEnviar";
+            this.btnEnviar.Size = new System.Drawing.Size(75, 23);
+            this.btnEnviar.TabIndex = 5;
+            this.btnEnviar.Text = "Enviar";
+            this.btnEnviar.UseVisualStyleBackColor = true;
+            this.btnEnviar.Click += new System.EventHandler(this.btnEnviar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(405, 325);
+            this.Controls.Add(this.btnEnviar);
+            this.Controls.Add(this.txtMensaje);
+            this.Controls.Add(this.lbTitulo1);
+            this.Controls.Add(this.btnConectar);
+            this.Controls.Add(this.txtUsuario);
+            this.Controls.Add(this.listBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.TextBox txtUsuario;
+        private System.Windows.Forms.Button btnConectar;
+        private System.Windows.Forms.Label lbTitulo1;
+        private System.Windows.Forms.TextBox txtMensaje;
+        private System.Windows.Forms.Button btnEnviar;
+    }
+}
+
---
+++ PIpeertopeer/PIpeertopeer/Form1.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using System.Threading;
+using System.Net.Sockets;
+using System.IO;
+using Transitions;
+
+
+
+namespace PIpeertopeer
+{
+    public partial class Form1 : Form
+    {
+        static private NetworkStream stream;
+        static private StreamWriter streamw;
+        static private StreamReader streamr;
+        static private TcpClient client = new TcpClient();
+        static private string nick = "unknown";
+
+
+        private delegate void DaddItem(String s);
+
+        private void AddItem(String s)
+        {
+            listBox1.Items.Add(s);
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        void Listen()
+        {
+            while (client.Connected)
+            {
+                try
+                {
+                    this.Invoke(new DaddItem(AddItem), streamr.ReadLine());
+                }
+                catch
+                {
+                    MessageBox.Show("No se ha podido conectar al servidor");
+                    Application.Exit();
+                }
+            }
+        }
+        void Conectar()
+        {
+            try
+            {
+                client.Connect("127.0.0.1", 8000);
+                if (client.Connected)
+                {
+                    Thread t = new Thread(Listen);
+
+                    stream = client.GetStream();
+                    streamw = new StreamWriter(stream);
+                    streamr = new StreamReader(stream);
+
+                    streamw.WriteLine(nick);
+                    streamw.Flush();
+
+
+                    t.Start();
+                }
+                else
+                {
+                    MessageBox.Show("Servidor no disponible");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Servidor no disponible");
+                Application.Exit();
+            }
+
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            btnEnviar.Location = new Point(-329, 250);
+            txtMensaje.Location = new Point(-329, 250);
+            listBox1.Location = new Point(-329, 23);
+
+        }
+        private void btnConectar_Click(object sender, EventArgs e)
+        {
+            nick = txtUsuario.Text;
+            Conectar();
+
+            Transition t = new Transition(new TransitionType_EaseInEaseOut(900));
+            t.add(lbTitulo1, "Left", 555);
+            t.add(txtUsuario, "Left", 555);
+            t.add(btnConectar, "Left", 555);
+            t.add(listBox1, "Left", 26);
+            t.add(txtMensaje, "Left", 26);
+            t.add(btnEnviar, "Left", 283);
+            t.run();
+        }
+
+        private void btnEnviar_Click(object sender, EventArgs e)
+        {
+            streamw.WriteLine(txtMensaje.Text);
+            streamw.Flush();
+            txtMensaje.Clear();
+        }
+    }
+}
